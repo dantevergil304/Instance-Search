f22c36f48552585c7d4082814a0b87225019d81f
dantevergil304 <voquochung304@gmail.com>
2018-12-16 01:00:07 +0700

    Add project tree and vid2shot.sh

---
+++ src/script/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+file="../../data/raw_data/info/master_shot_reference.txt.sample"
+while IFS= read -r line
+do
+	IFS="    "
+	set -- $line
+    name="../../data/raw_data/videos/$1"
+    start_time="$3"
+    end_time="$4"
+    output="../../data/raw_data/shots/$2.mp4"
+    echo $name
+    ffmpeg -nostdin -ss $start_time -i $name -to $end_time -c copy -copyts $output
+done <"$file"
