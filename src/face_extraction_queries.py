2be526b0723bad10e824ded59bf3deb07009299c
dantevergil304 <voquochung304@gmail.com>
2018-12-19 02:54:08 +0700

    Add 'detect face before apply mask' method for face detection in queries

--- src/face_extraction_queries.py
+++ src/face_extraction_queries.py
@@ -1,10 +1,12 @@
 from mtcnn.mtcnn import MTCNN
 
+import numpy as np
 import cv2
 import json
 import os
 import glob
 
+METHOD = 'detect_before_mask'
 
 if __name__ == "__main__":
 	with open('../cfg/config.json', 'r') as f:
@@ -23,17 +25,39 @@ if __name__ == "__main__":
 				'face', 'bmp'])
 
 		image = cv2.imread(query)
+		im_height, im_width = image.shape[0], image.shape[1]
 		mask = cv2.imread(os.path.join(queries_folder, maskname), 0)
 
-		res = cv2.bitwise_and(image, image, mask=mask)
+		if METHOD == 'mask_before_detect':
+			res = cv2.bitwise_and(image, image, mask=mask)
 		
-		# cv2.imshow('res', res)
-		# cv2.waitKey(0)
-		# cv2.destryAllWindows()
+			detected_faces = detector.detect_faces(res)
+			if len(detected_faces) > 0:
+				face_info = max(detected_faces, key=lambda x:x['confidence'])
+				x, y, w, h = face_info['box']
+				face = image[y : y + h, x : x + w]	
+				cv2.imwrite(os.path.join(queries_face_folder, METHOD, facename), face)
+		elif METHOD == 'detect_before_mask':
+			detected_faces = detector.detect_faces(image)
+			best_bbox = None
+			best_overlap = 0 
+			for face_info in detected_faces:
+				x, y, w, h = face_info['box']
+				bbox_mask = np.zeros((im_height, im_width), dtype=np.uint8)
+				cv2.rectangle(bbox_mask, (x, y), (x+w, y+h), 255, cv2.FILLED)
+				ones_mask = np.ones((im_height, im_width), dtype=np.uint8)
+				res = cv2.bitwise_and(mask, bbox_mask, mask=ones_mask)	
 
-		detected_faces = detector.detect_faces(res)
-		if len(detected_faces) > 0:
-			face_info = max(detected_faces, key=lambda x:x['confidence'])
-			x, y, w, h = face_info['box']
-			face = image[y : y + h, x : x + w]	
-			cv2.imwrite(os.path.join(queries_face_folder, facename), face)
+				overlap = np.count_nonzero(res)
+
+				if overlap > best_overlap:
+					best_bbox = face_info['box']
+					best_overlap = overlap
+				#cv2.imshow('fig', res)
+				#cv2.waitKey()
+				#cv2.destroyAllWindows()
+
+			if best_bbox is not None:
+				x, y, w, h = best_bbox 
+				face = image[y : y + h, x : x + w]	
+				cv2.imwrite(os.path.join(queries_face_folder, METHOD, facename), face)
