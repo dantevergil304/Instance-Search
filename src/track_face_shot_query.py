1493f8c577986c428d1e0e80423f2460e80ff418
dantvergil304 <voquochung304@gmail.com>
2019-06-06 14:01:16 +0700

    Update track face method

--- src/track_face_shot_query.py
+++ src/track_face_shot_query.py
@@ -8,6 +8,8 @@ import json
 import os
 import sys
 import tensorflow as tf
+import glob
+import xml.etree.ElementTree as ET
 
 
 sys.path.append(os.path.abspath(os.path.join('..', '3rd_party')))
@@ -46,12 +48,32 @@ def getCorrectFaceTrackInShot(topic_frame_path, topic_mask_path, shot_path):
     all_frames, topic_offset = getCorrectFrameInShot(
         topic_frame_path, shot_path)
 
-    topic_frame = all_frames[topic_offset]
+    # Create session for MTCNN
+    sess = tf.Session()
+    pnet, rnet, onet = lib.create_mtcnn(sess, None)
+
+    all_faces = []
+    all_bbs = []
+    for frame in all_frames:
+        faces, bbs = extract_faces_from_image(frame, pnet, rnet, onet)
+        all_faces.append(faces)
+        all_bbs.append(bbs)
+
+    topic_frame = cv2.imread(topic_frame_path)
     mask_frame = cv2.imread(topic_mask_path, 0)
-    mask_frame = cv2.resize(mask_frame, (768, 576))
+    # mask_frame = cv2.resize(mask_frame, (768, 576))
 
     _, bb, _ = detect_face_by_image([topic_frame], [mask_frame])
+    # face = face[0]
     bb = list(bb[0])
+    bb[0] /= (topic_frame.shape[1] / 768)
+    bb[1] /= (topic_frame.shape[0] / 576)
+    bb[2] /= (topic_frame.shape[1] / 768)
+    bb[3] /= (topic_frame.shape[0] / 576)
+    bb = [int(c) for c in bb]
+    topic_frame = cv2.resize(topic_frame, (768, 576))
+    mask_frame = cv2.resize(mask_frame, (768, 576))
+    face = topic_frame[bb[1]:bb[3], bb[0]:bb[2]]
 
     # params for ShiTomasi corner detection
     feature_params = dict(maxCorners=100,
@@ -61,7 +83,7 @@ def getCorrectFaceTrackInShot(topic_frame_path, topic_mask_path, shot_path):
 
     # Parameters for lucas kanade optical flow
     lk_params = dict(winSize=(15, 15),
-                     maxLevel=4,
+                     maxLevel=3,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
     old_topic_frame_gray = cv2.cvtColor(topic_frame, cv2.COLOR_BGR2GRAY)
@@ -69,130 +91,169 @@ def getCorrectFaceTrackInShot(topic_frame_path, topic_mask_path, shot_path):
     face_mask = np.zeros_like(old_topic_frame_gray)
     cv2.rectangle(face_mask, (bb[0], bb[1]), (bb[2], bb[3]), 255, -1)
 
+    # face_mask = cv2.bitwise_and(face_mask, mask_frame)
+
     p0 = cv2.goodFeaturesToTrack(
         old_topic_frame_gray, mask=face_mask, **feature_params)
+
+    old_topic_frame_gray_temp = old_topic_frame_gray.copy()
     p0_temp = p0
 
-    # Create session for MTCNN
-    sess = tf.Session()
-    pnet, rnet, onet = lib.create_mtcnn(sess, None)
+    tracked_faces = []
+    if face is not None:
+        tracked_faces.append(face)
 
     # Backward Tracking
     for idx, i in enumerate(range(topic_offset-1, -1, -1)):
         visualize_frame = all_frames[i].copy()
+        prev_frame_gray = cv2.cvtColor(all_frames[i+1], cv2.COLOR_BGR2GRAY)
         frame_gray = cv2.cvtColor(all_frames[i], cv2.COLOR_BGR2GRAY)
+
+        diff = np.sum((prev_frame_gray - frame_gray) ** 2) / (frame_gray.shape[0] * frame_gray.shape[1])
+        if diff > 80: 
+            break 
+
+        frame_gray = cv2.equalizeHist(frame_gray)
 
         # calculate optical flow
         p1, st, err = cv2.calcOpticalFlowPyrLK(
             old_topic_frame_gray, frame_gray, p0, None, **lk_params)
+        if st is None:
+            break
 
         good_new = p1[st == 1]
         good_old = p0[st == 1]
 
-        avg_x = 0
-        avg_y = 0
+        features_mask = np.zeros_like(frame_gray, dtype=np.uint8)
         for new, old in zip(good_new, good_old):
             x_new, y_new = new.ravel()
             x_old, y_old = old.ravel()
             cv2.circle(visualize_frame, (x_new, y_new), 2, (0, 255, 0), -1)
+            cv2.circle(features_mask, (x_new, y_new), 0, 255, 0)
+        # cv2.imshow('feature mask', features_mask)
 
-            avg_x += (x_new - x_old)
-            avg_y += (y_new - y_old)
-        avg_x = int(avg_x / len(good_new))
-        avg_y = int(avg_y / len(good_new))
+        faces_mask = np.zeros_like(frame_gray, dtype=np.uint8)
+        for bb in all_bbs[i]:
+            left, top, right, bottom = extendBB(
+                frame_gray.shape[:2], bb[0], bb[1], bb[2], bb[3], 0)
+            cv2.rectangle(faces_mask, (left, top), (right, bottom), 255, -1)
+        # cv2.imshow('faces mask', faces_mask)
 
-        # Update Bbox
-        bb[0] += avg_x
-        bb[1] += avg_y
-        bb[2] += avg_x
-        bb[3] += avg_y
+        features_in_bb_mask = cv2.bitwise_and(features_mask, faces_mask)
+        # cv2.imshow('feature in bb', features_in_bb_mask)
 
+        features_in_bb_coord = np.where(features_in_bb_mask == 255)
+        selectedFace = None
+        selectedBB = None
+        for px in zip(features_in_bb_coord[1], features_in_bb_coord[0]):
+            x, y = px
+            for face, bb in zip(all_faces[i], all_bbs[i]):
+                if x >= bb[0] and x <= bb[2] and y >= bb[1]and y <= bb[3]:
+                    selectedFace = face
+                    selectedBB = bb
 
-        cv2.rectangle(visualize_frame, (bb[0], bb[1]), (bb[2], bb[3]), (0, 255, 0), 2)
-        cv2.imshow('forward tracking', visualize_frame)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+        if selectedBB is not None:
+            cv2.rectangle(visualize_frame, (selectedBB[0], selectedBB[1]), (
+                selectedBB[2], selectedBB[3]), (0, 255, 0), 2)
+            tracked_faces.insert(0, selectedFace)
+        # cv2.imshow('forward tracking', visualize_frame)
 
-        if idx % 5 == 0:
-        #     all_frames[i] = cv2.cvtColor(all_frames[i], cv2.COLOR_BGR2RGB)
-        #     print("here", all_frames[i].shape)
-        #     _, bbs = extract_faces_from_image(all_frames[i], pnet, rnet, onet)
-        #     all_frames[i] = cv2.cvtColor(all_frames[i], cv2.COLOR_RGB2BGR)
-        #     
-        #     temp = all_frames[i].copy()
-        #     for bbox in bbs:
-        #         cv2.rectangle(temp, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
-        #     cv2.imshow('fig', temp)
-        #     cv2.waitKey()
-        #     cv2.destroyAllWindows()
+        # mask_img = np.hstack((features_mask, faces_mask))
+        # result_img = np.hstack((features_in_bb_mask, frame_gray))
+        # visualize_img = np.vstack((mask_img, result_img))
+        # cv2.imshow('visualize img', visualize_img)
 
-        #     best_fit_bbox = None
-        #     best_cnt_inlier = -np.inf 
-        #     for bbox in bbs:
-        #         cnt_inlier = 0
-        #         for new in good_new:
-        #             x, y = new.ravel()
-        #             if x >= bbox[0] and x <= bbox[2] and y >= bbox[1] and y <= bbox[3]:
-        #                 cnt_inlier += 1 
-
-        #         if cnt_inlier > best_cnt_inlier:
-        #             best_fit_bbox = bbox
-        #             best_cnt_inlier = cnt_inlier
-
-        #     bb = list(best_fit_bbox)
-            face_mask = np.zeros_like(old_topic_frame_gray)
-            cv2.rectangle(face_mask, (bb[0], bb[1]), (bb[2], bb[3]), 255, -1)
-            new_corner = cv2.goodFeaturesToTrack(
-                frame_gray, mask=face_mask, **feature_params)
-
-            good_new = good_new.tolist()
-            new_corner = new_corner.squeeze().tolist()
-            good_new.extend(new_corner)
-            good_new = np.array(good_new, np.float32)
-            
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
 
         old_topic_frame_gray = frame_gray.copy()
         p0 = good_new.reshape(-1, 1, 2)
 
     # Forward Tracking
-    # p0 = p0_temp
-    # for i in range(topic_offset+1, len(all_frames)):
-    #     frame_gray = cv2.cvtColor(all_frames[i], cv2.COLOR_BGR2GRAY)
+    old_topic_frame_gray = old_topic_frame_gray_temp
+    p0 = p0_temp
+    for idx, i in enumerate(range(topic_offset+1, len(all_frames))):
+        visualize_frame = all_frames[i].copy()
+        prev_frame_gray = cv2.cvtColor(all_frames[i-1], cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.cvtColor(all_frames[i], cv2.COLOR_BGR2GRAY)
 
-    #     # calculate optical flow
-    #     p1, st, err = cv2.calcOpticalFlowPyrLK(
-    #         old_topic_frame_gray, frame_gray, p0, None, **lk_params)
+        diff = np.sum((prev_frame_gray - frame_gray) ** 2) / (frame_gray.shape[0] * frame_gray.shape[1])
+        if diff > 80: 
+            break 
 
-    #     good_new = p1[st == 1]
-    #     good_old = p0[st == 1]
+        frame_gray = cv2.equalizeHist(frame_gray)
 
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(
+            old_topic_frame_gray, frame_gray, p0, None, **lk_params)
+        if st is None:
+            break
 
-    #     avg_x = 0
-    #     avg_y = 0
-    #     for new, old in zip(good_new, good_old):
-    #         x_new, y_new = new.ravel()
-    #         x_old, y_old = old.ravel()
-    #         cv2.circle(all_frames[i], (x_new, y_new), 2, (0, 255, 0), -1)
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
 
-    #         avg_x += (x_new - x_old)
-    #         avg_y += (y_new - y_old)
-    #     avg_x = int(avg_x / len(good_new))
-    #     avg_y = int(len(good_new))
+        features_mask = np.zeros_like(frame_gray, dtype=np.uint8)
+        for new, old in zip(good_new, good_old):
+            x_new, y_new = new.ravel()
+            x_old, y_old = old.ravel()
+            # cv2.circle(frame_gray, (x_new, y_new), 2, (0, 255, 0), -1)
+            cv2.circle(features_mask, (x_new, y_new), 0, 255, 0)
+        # cv2.imshow('feature mask', features_mask)
 
-    #     # Update Bbox
-    #     print(bb)
-    #     bb[0] += avg_x
-    #     bb[1] += avg_y
-    #     bb[2] += avg_x
-    #     bb[3] += avg_y
+        faces_mask = np.zeros_like(frame_gray, dtype=np.uint8)
+        for bb in all_bbs[i]:
+            left, top, right, bottom = extendBB(
+                frame_gray.shape[:2], bb[0], bb[1], bb[2], bb[3], 0)
+            cv2.rectangle(faces_mask, (left, top), (right, bottom), 255, -1)
+        # cv2.imshow('faces mask', faces_mask)
 
-    #     cv2.rectangle(all_frames[i], (bb[0], bb[1]), (bb[2], bb[3]), (0, 255, 0), 2)
-    #     cv2.imshow('forward tracking', all_frames[i])
-    #     cv2.waitKey()
-    #     cv2.destroyAllWindows()
+        features_in_bb_mask = cv2.bitwise_and(features_mask, faces_mask)
+        # cv2.imshow('feature in bb', features_in_bb_mask)
 
-    #     old_topic_frame_gray = frame_gray.copy()
-    #     p0 = good_new.reshape(-1, 1, 2)
+        features_in_bb_coord = np.where(features_in_bb_mask == 255)
+        selectedFace = None
+        selectedBB = None
+        for px in zip(features_in_bb_coord[1], features_in_bb_coord[0]):
+            x, y = px
+            for face, bb in zip(all_faces[i], all_bbs[i]):
+                if x >= bb[0] and x <= bb[2] and y >= bb[1]and y <= bb[3]:
+                    selectedFace = face
+                    selectedBB = bb
+
+        if selectedBB is not None:
+            cv2.rectangle(visualize_frame, (selectedBB[0], selectedBB[1]), (
+                selectedBB[2], selectedBB[3]), (0, 255, 0), 2)
+            tracked_faces.append(selectedFace)
+        # cv2.imshow('forward tracking', visualize_frame)
+
+        # mask_img = np.hstack((features_mask, faces_mask))
+        # result_img = np.hstack((features_in_bb_mask, frame_gray))
+        # visualize_img = np.vstack((mask_img, result_img))
+        # cv2.imshow('visualize img', visualize_img)
+
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        old_topic_frame_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+
+    default_face_height = 50
+    visualize_faces = None
+    for face in tracked_faces:
+        height, width = face.shape[:2]
+        face_width = int(default_face_height * width / height)
+
+        face = cv2.resize(face, (face_width, default_face_height))
+
+        if visualize_faces is None:
+            visualize_faces = face
+        else:
+            visualize_faces = np.hstack((visualize_faces, face))
+
+    # cv2.imshow('visualize face track', visualize_faces)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+    return tracked_faces, visualize_faces
 
 
 if __name__ == '__main__':
@@ -202,20 +263,65 @@ if __name__ == '__main__':
 
     query_folder = cfg['raw_data']['queries_folder']
     query_shot_folder = cfg['raw_data']['shot_example_folder']
+    info_folder = cfg['raw_data']['info_folder']
 
-    test_topic_frame_path = os.path.join(query_folder, 'chelsea.3.src.png')
-    test_topic_mask_path = os.path.join(query_folder, 'chelsea.3.mask.png')
-    test_shot_path = os.path.join(
-        query_shot_folder, 'chelsea', 'shot0_1146.mp4')
-    topic_frame = cv2.imread(test_topic_frame_path)
-    topic_frame = cv2.resize(topic_frame, (768, 576))
+    # test_topic_frame_path = os.path.join(query_folder, 'heather.1.src.png')
+    # test_topic_mask_path = os.path.join(query_folder, 'heather.1.mask.png')
+    # test_shot_path = os.path.join(
+    #     query_shot_folder, 'heather', 'shot0_769.mp4')
+    # topic_frame = cv2.imread(test_topic_frame_path)
+    # topic_frame = cv2.resize(topic_frame, (768, 576))
 
-    all_frames, topic_offset = getCorrectFrameInShot(
-        test_topic_frame_path, test_shot_path)
+    # all_frames, topic_offset = getCorrectFrameInShot(
+    #     test_topic_frame_path, test_shot_path)
 
-    # cv2.imshow('found frame', all_frames[topic_offset])
-    # cv2.imshow('gt frame', topic_frame)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
-    getCorrectFaceTrackInShot(test_topic_frame_path,
-                              test_topic_mask_path, test_shot_path)
+    # # cv2.imshow('found frame', all_frames[topic_offset])
+    # # cv2.imshow('gt frame', topic_frame)
+    # # cv2.waitKey()
+    # # cv2.destroyAllWindows()
+    # getCorrectFaceTrackInShot(test_topic_frame_path,
+    #                           test_topic_mask_path, test_shot_path)
+
+
+
+    topic_file = os.path.join(info_folder, 'ins.auto.topics.2018.xml')
+    print(topic_file)
+    tree = ET.parse(topic_file)
+    root = tree.getroot()
+
+    info_dict = dict()
+    for topic in root.findall('videoInstanceTopic'):
+        for image in topic.findall('imageExample'):
+            info_dict[image.attrib['src']] = image.attrib['shotID']
+
+    names = ['chelsea', 'darrin', 'garry', 'heather',
+             'jane', 'jack', 'max', 'minty', 'mo', 'zainab']
+    # names = ['chelsea']
+    for name in names:
+        for i in range(1, 5):
+            topic_frame_path = os.path.join(
+                query_folder, f'{name}.{i}.src.png')
+            topic_mask_path = os.path.join(
+                query_folder, f'{name}.{i}.mask.png')
+            shot_path = os.path.join(
+                query_shot_folder, f'{name}', info_dict[f'{name}.{i}.src.png'] + '.mp4')
+
+            print(topic_frame_path, topic_mask_path, shot_path)
+
+            track_faces, visualize_faces = getCorrectFaceTrackInShot(
+                topic_frame_path, topic_mask_path, shot_path)
+
+            shot_face_folder = os.path.join(
+                query_folder, 'shot_query_faces', f'{name}', f'{i}')
+            if not os.path.exists(shot_face_folder):
+                os.makedirs(shot_face_folder, exist_ok=True)
+            for idx, face in enumerate(track_faces):
+                cv2.imwrite(os.path.join(shot_face_folder,
+                                         f'{name}.{idx}.face.png'), face)
+
+            visualize_face_folder = os.path.join(
+                query_folder, 'visualize_shot_query_faces', f'{name}')
+            if not os.path.exists(visualize_face_folder):
+                os.mkdir(visualize_face_folder)
+            cv2.imwrite(os.path.join(visualize_face_folder,
+                                     f'facetrack.{i}.png'), visualize_faces)
